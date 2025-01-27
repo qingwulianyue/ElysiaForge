3fb263209d8449d7e26a05f3ba96e7186eecd90f
Elysia <2569736791@qq.com>
2025-01-27 12:05:45 +0800

    基础项目架构以及初步配置文件

---
+++ src/main/java/main/elysiaforge/ElysiaForge.java
@@ -0,0 +1,17 @@
+package main.elysiaforge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ElysiaForge extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
